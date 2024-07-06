db1a7f8a16e3ffbd0dd142a641b2d98609aadbff
eq111 <kra199re@gmail.com>
2024-07-06 08:40:22 +0300

    First Publication

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("myslides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+
+function showSidebar(){
+  const sidebar = document.querySelector('.navigater')
+  sidebar.style.display = 'block'
+  }
+  function hideSidebar(){
+  const sidebar = document.querySelector('.navigater')
+  sidebar.style.display = 'none'
+  }
+
+
+
