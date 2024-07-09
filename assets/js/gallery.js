66abbf555dbe4d6385b0efc75473bc011a8d062f
eq111 <kra199re@gmail.com>
2024-07-09 07:51:52 +0300

    refine_2

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,13 +1,10 @@
 
-
 function GFG_click(clicked){
 
+  var currentpic = clicked
   var modal = document.getElementById("myModal");
-
-  var img = document.getElementById(clicked);
+  var img = document.getElementById(currentpic);
   var modalImg = document.getElementById("img01");
-
-  img.innerHTML.src
   
   modal.style.display = "block";
   modalImg.src = img.src;
@@ -19,6 +16,28 @@ function GFG_click(clicked){
 }
 
 
+/*const thepics = document.getElementsByClassName("slide");
+var imgageindex = thepics.findIndex(obj => obj.id === img.id);
+var modalImg = document.getElementById("img01");
 
+console.log(Array.isArray("thepics"));
 
+var a = document.getElementsByClassName("prev");
+  function plusSlides() {
+    modalImg.src = thepics[1].src;
+  }
 
+console.log(pic1d);
+slideid = pic1d;
+var thepics = document.getElementsByClassName("slide");
+var theelement = document.getElementById(slideid);
+var imgageindex = thepics.indexOf(theelement);
+
+function plusSlides(){
+  alert(thepics[imgageindex += 1])
+  modalImg.src = thepics[imgageindex += 1].src
+}
+
+function minusSlides(){
+  modalImg.src = thepics[imgageindex -= 1].src
+}*/
